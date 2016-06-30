140a23cdca5f98107aadb0580b07cb788bb08cfb
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-06-30 10:18:17 +0200

    Refactors rally directives and moves each to a separated file

---
+++ public/app/modules/mobidul/station/directives/inputcode.directive.js
@@ -0,0 +1,45 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('Mobidul')
+    .direvtice('inputcode', inputCode);
+
+  inputCode.$inject = [
+    '$rootScope'
+  ];
+
+  function inputCode(
+    $rootScope
+  ){
+    return {
+      restrict: "E",
+      scope:{
+        verifier: "@",
+        success: '@',
+        error: '@'
+      },
+      template: "<div><form ng-submit='ctrl.submit()'><md-input-container><input ng-model='ctrl.input' data-success='{{success}}' data-error='{{error}}'</input></md-input-container><md-button type='submit' class='md-raised md-primary'>Go</md-button></form></div>",
+
+      controller: function($scope, $element, $attrs){
+        var ctrl = this;
+
+        ctrl.submit = submit;
+
+        function submit(){
+
+          if(ctrl.input){
+            if(ctrl.input.toLowerCase() == $attrs.verifier.toLowerCase()){
+              $rootScope.$broadcast('action', $attrs.success);
+            }else{
+              $rootScope.$broadcast('action', $attrs.error);
+            }
+          }
+
+        }
+      },
+      controllerAs: 'ctrl'
+    };
+  }
+
+})();
