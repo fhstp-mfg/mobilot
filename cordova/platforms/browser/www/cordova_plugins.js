aad2f7c34746bcba25be9f38fb318fd119153bb2
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-07-04 16:07:57 +0200

    Updates Cordova installation (see #2)

--- cordova/platforms/browser/www/cordova_plugins.js
+++ cordova/platforms/browser/www/cordova_plugins.js
@@ -3,6 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-geolocation": "2.2.0",
     "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
