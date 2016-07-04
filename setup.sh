68de9d285e35b9bcb6911737229da9e748ec4b49
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-07-04 11:46:56 +0200

    Adds `setup.sh` for initial setup (see #3)

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 'Creating folders under "app/storage/"...'
+mkdir app/storage/cache
+mkdir app/storage/logs
+mkdir app/storage/meta
+mkdir app/storage/sessions
+mkdir app/storage/views
+echo 'Creating folders under "public/"...'
+mkdir public/upload
+echo 'Running "composer install"...'
+composer install
+
+echo 'NOTE: Manually create "database.php" (if necessary)'
+echo 'NOTE: Manually create database named "mobilot" (MySQL)'
