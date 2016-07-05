de033f783b0fbe6201be8d91d912ee77a39bf5ea
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-07-05 15:12:21 +0200

    Prepares AnalyticsService file (refs #5)

---
+++ public/app/modules/common/AnalyticsService.js
@@ -0,0 +1,33 @@
+angular
+  .module('Mobilot')
+  .factory('AnalyticsService', AnalyticsService);
+
+AnalyticsService.$inject = [
+  '$log'
+];
+
+
+function AnalyticsService (
+  $log
+) {
+  /// AnalyticsService
+  var service =
+  {
+    // constants
+    // ...
+
+    // vars
+    // ...
+
+    // services
+    // ...
+  }
+
+
+  /// services
+
+  // ...
+
+
+  return service;
+}
