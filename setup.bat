372675c0c79cd7ee1e328a875ecc64e665fd13c6
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-07-05 15:03:21 +0200

    Quick fixes setup.bat

--- setup.bat
+++ setup.bat
@@ -1,24 +1,14 @@
 @ECHO OFF
-Title Mobilot Setup
 
-ECHO Running "composer install" ...
-composer install
+echo.
+echo Welcome %USERNAME%!
+echo "This setup will guide you through the process of installing mobiloton your machine."
+echo.
 
-<<<<<<< HEAD
 SET /p answer= ^> Have you created a MySQL database named "mobilot"? (Y/n)
-=======
-ECHO Creating folders under "app/storage/" ...
-mkdir app\storage\logs
-mkdir app\storage\cache
-mkdir app\storage\meta
-mkdir app\storage\sessions
-mkdir app\storage\views
->>>>>>> 06682382d07acfec9edeb619a772afa727895c55
 
-ECHO Creating folders under "public/" ...
-mkdir public\upload
+IF NOT "%answer%"=="n" (
 
-<<<<<<< HEAD
   echo.
   echo ^> Creating folders under "app/storage/" ...
   mkdir app\storage\logs
@@ -46,7 +36,7 @@ IF "%answer%"=="n" (
 )
 
 :CopyDatabasePHP
-SET /p answer2= ^> What kind of environemnt are you using? (H)omestead/(X)AMPP: 
+SET /p answer2= ^> What kind of environemnt are you using? (H)omestead/(X)AMPP:
 SET location = ""
 IF "%answer2%"=="H" (
   SET location=app\config\local
@@ -63,14 +53,14 @@ echo ^> Copying "setup\database.php" to "%location%"
 @xcopy /s /I setup %location%
 
 echo.
-SET /p answer3= ^> IMPORTANT: Configure %location% before continuning (press ENTER) 
+SET /p answer3= ^> IMPORTANT: Configure %location% before continuning (press ENTER)
 echo.
 call php artisan migrate
 
 echo.
 echo ^> Mobilot setup completed!
 
-SET /p answer4= ^> Would you like to serve Mobilot now? (Y/n) 
+SET /p answer4= ^> Would you like to serve Mobilot now? (Y/n)
 
 IF NOT "%answer4%"=="n" (
   echo ^> php artisan serve
@@ -82,7 +72,3 @@ IF "%answer4%"=="n" (
 
 echo ^> Good bye!
 exit /b 0
-=======
-ECHO NOTE: Manually create "database.php" (if necessary)
-ECHO NOTE: Manually create database named "mobilot" (MySQL)
->>>>>>> 06682382d07acfec9edeb619a772afa727895c55
