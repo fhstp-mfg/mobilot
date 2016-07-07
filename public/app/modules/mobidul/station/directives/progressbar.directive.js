2a556fc2b229ec536c42517ff4ac3f46a309bd79
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-07-07 09:32:05 +0200

    Fixes Styles of progress bar if there are more than ten stations

--- public/app/modules/mobidul/station/directives/progressbar.directive.js
+++ public/app/modules/mobidul/station/directives/progressbar.directive.js
@@ -21,14 +21,20 @@
         current: '@',
         length: '@'
       },
-      template: '<div >' +
-        '<ul class="progressbar clearfix" data-progtrckr-steps="{{ length }}">' +
+      template: '<div>' +
+        '<ul id="progressbar_container" class="progressbar clearfix" data-progtrckr-steps="{{ length }}">' +
         '<li data-ng-class="($index <= current) ? \'finished\' : \'\'" '+
             'data-ng-repeat="i in ctrl.getNumber(length) track by $index"' +
             'data-ng-click="ctrl.goToStation($index)"></li>'+
         '</ul></div>',
       link: function($scope, $element, $attrs, ctrl){
         $attrs.current = parseInt($attrs.current);
+
+        $scope.length = parseInt($scope.length);
+
+        if($scope.length > 10){
+          document.getElementById('progressbar_container').style.width = ($scope.length * 80) + 'px';
+        }
 
       },
       controller: function($scope, $element, $attrs){
