891ebddc43be511ef72940027adf2074e27647f9
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-07-18 11:38:34 +0200

    Code revision "add componentId to attachment table" migration

--- app/database/migrations/2016_07_15_164015_add_componentId_to_attachment_table.php
+++ app/database/migrations/2016_07_15_164015_add_componentId_to_attachment_table.php
@@ -5,30 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddComponentIdToAttachmentTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('attachment', function(Blueprint $table)
-        {
-           $table->string('componentId')->nullable();
-        });
-	}
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('attachment', function(Blueprint $table)
+    {
+      $table->string('componentId')->nullable();
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-	    Schema::table('attachmdnt', function(Blueprint $table)
-        {
-           $table->dropColumn('componentId');
-        });
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('attachmdnt', function(Blueprint $table)
+    {
+      $table->dropColumn('componentId');
+    });
+  }
 
 }
