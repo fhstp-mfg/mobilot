acc3e78f50197b5543358bd10d6bfe1ae61e72ad
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-07-18 11:43:56 +0200

    Code revision InputCode element (refs #24)

--- public/app/modules/mobidul/station/elements/InputCodeDirective.js
+++ public/app/modules/mobidul/station/elements/InputCodeDirective.js
@@ -1,51 +1,49 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular
-    .module('Mobidul')
-    .directive('mblInputCode', InputCode);
+angular
+  .module('Mobidul')
+  .directive('mblInputCode', InputCode);
 
-  InputCode.$inject = [
-    '$log',
-    '$rootScope'
-  ];
+InputCode.$inject = [
+  '$log',
+  '$rootScope'
+];
 
-  function InputCode(
-    $log,
-    $rootScope
-  ){
-    return {
-      restrict: "E",
-      scope:{
-        verifier: "@",
-        success: '@',
-        error: '@'
-      },
-      template: "<div><form ng-submit='ctrl.submit()'><md-input-container><input ng-model='ctrl.input' data-success='success' data-error='error'</input></md-input-container><md-button type='submit' class='md-raised md-primary'>Go</md-button></form></div>",
+function InputCode (
+  $log,
+  $rootScope
+) {
+  return {
+    restrict: 'E',
+    scope: {
+      verifier: '@',
+      success: '@',
+      error: '@'
+    },
+    template: "<div><form ng-submit='ctrl.submit()'><md-input-container><input ng-model='ctrl.input' data-success='success' data-error='error'</input></md-input-container><md-button type='submit' class='md-raised md-primary'>Go</md-button></form></div>",
 
-      link: function($scope, $element, $attrs, ctrl){
+    link: function ($scope, $element, $attrs, ctrl) {
+      // ...
+    },
 
-      },
+    controller: function ($scope, $element, $attrs) {
+      var ctrl = this;
 
-      controller: function($scope, $element, $attrs){
-        var ctrl = this;
+      ctrl.submit = submit;
 
-        ctrl.submit = submit;
+      function submit () {
 
-        function submit(){
-          
-          if(ctrl.input){
-            if(ctrl.input.toLowerCase() == $scope.verifier.toLowerCase()){
-              $rootScope.$broadcast('action', $scope.success);
-            }else{
-              $rootScope.$broadcast('action', $scope.error);
-            }
+        if (ctrl.input) {
+          if (ctrl.input.toLowerCase() == $scope.verifier.toLowerCase()) {
+            $rootScope.$broadcast('action', $scope.success);
+          } else {
+            $rootScope.$broadcast('action', $scope.error);
           }
-
         }
-      },
-      controllerAs: 'ctrl'
-    };
+      }
+    },
+    controllerAs: 'ctrl'
   }
-
+}
 })();
