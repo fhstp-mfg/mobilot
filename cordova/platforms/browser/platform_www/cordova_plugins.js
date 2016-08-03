11f8e4dee2fd38c6fecca2f243309e8bdb518d8f
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-08-03 18:56:38 +0200

    Adds phonegap-plugin-barcodescanner v6.0.1 by kotikov.vladimir (refs #44)

--- cordova/platforms/browser/platform_www/cordova_plugins.js
+++ cordova/platforms/browser/platform_www/cordova_plugins.js
@@ -1,11 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/src/browser/BarcodeScannerProxy.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScannerProxy",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-compat": "1.0.0",
     "cordova-plugin-geolocation": "2.2.0",
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "phonegap-plugin-barcodescanner": "6.0.1"
 }
 // BOTTOM OF METADATA
 });
