dac792f3ee5e65aced14767df0ce5015b187d0ae
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-10-24 17:30:05 +0200

    Tries fixing Media preview (w/ no luck) (refs #126)

--- public/app/modules/mobidul/station/media/MediaController.js
+++ public/app/modules/mobidul/station/media/MediaController.js
@@ -1,42 +1,40 @@
 angular
-	.module('Mobidul')
-	.controller('MediaController', MediaController);
+  .module('Mobidul')
+  .controller('MediaController', MediaController);
 
 MediaController.$inject = [
-	'$log', '$stateParams', '$rootScope'
+  '$log', '$stateParams', '$rootScope'
 ];
 
 
-function MediaController ($log, $stateParams, $rootScope)
-{
-	var media = this;
+function MediaController ($log, $stateParams, $rootScope) {
+  var media = this;
 
-	// vars
-    media.src ="image/2000/"+$stateParams.media;
-    media.panzoomConfig = {
-            scalePerZoomLevel   : 1.5,
-            initialZoomLevel    : 0.25
-        };
-    
-//        $scope.panzoomConfig = {
-//            zoomLevels: 12,
-//            neutralZoomLevel: 5,
-//            scalePerZoomLevel: 1.5,
-//            initialZoomToFit: shark
-//        };
-    
-    media.height = window.screen.availHeight - 164;
-    media.panzoomModel = {};
+  // vars
+  media.src = cordovaUrl + '/image/2000/' + $stateParams.media;
+  media.panzoomConfig = {
+    scalePerZoomLevel : 1.5,
+    initialZoomLevel  : 0.25
+  };
 
-	// functions
-	init();
+  // $scope.panzoomConfig = {
+  //   zoomLevels: 12,
+  //   neutralZoomLevel: 5,
+  //   scalePerZoomLevel: 1.5,
+  //   initialZoomToFit: shark
+  // };
 
-	/// functions
-	function init ()
-	{
-        // $log.debug("got to mediacontroller"); 
-        // $log.debug($stateParams);
-        
-        $rootScope.$emit('rootScope:toggleAppLoader', { action : 'hide' });
-	}
+  media.height = window.screen.availHeight - 164;
+  media.panzoomModel = {};
+
+  // functions
+  init();
+
+  /// functions
+  function init () {
+    // $log.debug("got to mediacontroller");
+    // $log.debug($stateParams);
+
+    $rootScope.$emit('rootScope:toggleAppLoader', { action : 'hide' });
+  }
 }
