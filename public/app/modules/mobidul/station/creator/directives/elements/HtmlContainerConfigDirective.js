464c131763283b2d9d335e94d5b3d9ec90111441
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-12-05 15:23:54 +0100

    Visually improves HtmlContainer

--- public/app/modules/mobidul/station/creator/directives/elements/HtmlContainerConfigDirective.js
+++ public/app/modules/mobidul/station/creator/directives/elements/HtmlContainerConfigDirective.js
@@ -21,7 +21,11 @@ function HtmlEditor (
     },
     template: '' +
       '<div>' +
-        '<div ng-bind-html="htmlEditor.trustAsHtml(content)" class="editor-preview"></div>' +
+        '<div ' +
+          'ng-bind-html="htmlEditor.trustAsHtml(content)" ' +
+          'class="editor-preview" ' +
+          'style="padding: 1rem"' +
+        '></div>' +
         '<wysiwyg-edit ' +
           'content="content" ' +
           'api="htmlEditor.api">' +
