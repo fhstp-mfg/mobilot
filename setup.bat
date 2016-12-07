def5ceab4b7d62c7ae03b23cd6be693b39bd4211
doomsayer2 <fgrassinger@gmx.at>
2016-12-07 11:00:12 +0100

    Updates setup.bat for Windows to be nicer (closes #146)

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 
 echo.
-echo Welcome %USERNAME%!
+echo Hello %USERNAME%!
+echo.
 echo "This setup will guide you through the process of installing mobilot on your machine."
 echo.
 
@@ -10,7 +11,7 @@ SET /p answer= ^> Have you created a MySQL database named "mobilot"? (Y/n)
 IF NOT "%answer%"=="n" (
 
   echo.
-  echo ^> Creating folders under "app/storage/" ...
+  echo ^> Creating storage folders "app/storage/" ...
   mkdir app\storage\logs
   mkdir app\storage\cache
   mkdir app\storage\meta
@@ -18,11 +19,12 @@ IF NOT "%answer%"=="n" (
   mkdir app\storage\views
 
   echo.
-  echo ^> Creating folders under "public/" ...
+  echo ^> Creating uploads folder "public/" ...
   mkdir public\upload
 
   echo.
   echo ^> composer install
+  echo One moment please ...
   call composer install
 
   echo.
@@ -31,7 +33,7 @@ IF NOT "%answer%"=="n" (
 )
 
 IF "%answer%"=="n" (
-  echo ^>  Create a database named "mobilot" and than run this script again
+  echo ^>  Create a database named "mobilot" and than run this script again.
   exit /b
 )
 
@@ -53,12 +55,34 @@ echo ^> Copying "setup\database.php" to "%location%"
 @xcopy /s /I setup %location%
 
 echo.
-SET /p answer3= ^> IMPORTANT: Configure %location% before continuing (press ENTER)
+SET /p answer3= ^> IMPORTANT: Configure %location% before continuing (press ENTER). Note that for some local environments (such as MAMP) you might need to specify a unix_socket.
+
 echo.
+echo ^> php artisan migrate
+echo One moment please ...
 call php artisan migrate
 
 echo.
+SET /p answer5= ^> Finally let us take a deep breath ... ... and install npm and bower modules! (press ENTER)
+echo ^> npm install
+npm install
+
+echo ^> bower install
+echo "When asked, always choose the later module versions required by Mobilot."
+cd public
+bower install
+
+echo.
 echo ^> Mobilot setup completed!
+
+echo.
+echo Hints:
+echo ^> To manually serve Mobilot run: php artisan serve
+echo ^> If something went wrong on the way like mistyping or misconfiguring the "database.php", you can confidently try repeating the process.
+echo.
+echo Further help  – https://github.com/fhstp-mfg/mobilot/wiki/Setup
+echo Report issues – https://github.com/fhstp-mfg/mobilot/issues
+echo.
 
 SET /p answer4= ^> Would you like to serve Mobilot now? (Y/n) 
 
@@ -70,5 +94,5 @@ IF "%answer4%"=="n" (
   echo ^> INFO: To serve Mobilot run: "php artisan serve"
 )
 
-echo ^> Good bye!
+echo ^> Good bye then :)
 exit /b 0
