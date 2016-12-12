78f1930dc43e2b7a672ddfddd189b1530118fa3a
Iosif Miclaus <iosif.miclaus@gmail.com>
2016-12-12 18:57:04 +0100

    Add build iOS script ( @doomsayer2 you could use this for build_android.sh, if suited )

---
+++ build_ios.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Building Mobilot for iOS !"
+
+cd public/
+
+echo "> Run production build task"
+gulp build
+
+cd ../
+
+echo "> Copy files for Cordova iOS"
+python cordova_prepare_ios.py
+
+cd cordova/
+
+echo "> Cordova build iOS version"
+cordova build ios
+
+echo "> Open Xcode project"
+open platforms/ios/Mobilot.xcodeproj
+
+cd ../
+
+echo "Done building Mobilot for iOS !'
